4e4552001025605d7c76483f145cbc17b122dd3f
Khethelo Nkosi <khnkosi022@student.wethinkcode.co.za>
2023-03-02 15:06:16 +0200

    Testing out JS data types

--- myScript.js
+++ myScript.js
@@ -10,3 +10,19 @@ function calculate() {
     z = "six";
     window.alert("3 plus 3 is: "+z+"!");
 }
+
+function dataTypes() {
+    let words = ['feel', 'fire', 'fox', 'funny'];
+    // document.getElementById('datatypes').innerHTML= typeof words;
+
+    let car = {
+        brand:"Porsche",
+        model:"911 Carrera S",
+        year:1967,
+        color:"Powder Black",
+        fullRecap: function() {
+            return "A "+this.color+" "+this.year+" "+this.brand+", model: "+this.model;
+        }
+    };
+    document.getElementById('datatypes').innerHTML = car.fullRecap();
+}
