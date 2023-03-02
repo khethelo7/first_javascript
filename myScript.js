c9dc8f4d35fb291afd75cba8fae435d7d725a85e
khethelo7 <saneleknkosi@gmail.com>
2023-03-02 13:48:26 +0200

    Second Commit

---
+++ myScript.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.getElementById('demo').innerHTML = "Paragraph changed.";
+}
+
+function calculate() {
+    let x, y, z;
+    x = 3;
+    y = 3;
+    z = x+y;
+    z = "six";
+    window.alert("3 plus 3 is: "+z+"!");
+}
